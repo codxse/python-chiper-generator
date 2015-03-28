b6b0452271ed906b56d9d313baef4ee616bda3b0
Nadiar AS <nadiar429@gmail.com>
2015-03-28 12:01:02 +0700

    tambah modul 126 ASCII karakter enkripsi

--- cipy.py
+++ cipy.py
@@ -1,5 +1,26 @@
 import sys
 
+def encrypt(key):
+	'''
+	126 was using forr ASCII Printable Character map
+	http://thelivingpearl.com/printable-ascii-characters/
+	'''
+	plaintext = raw_input('Masukan pesan: ')
+	cipher = ''
+
+	for char in plaintext:
+		c = (ord(char) + key) % 126
+
+		if c < 32:
+			''' 
+			First 32 was some unprintable characters
+			'''
+			c += 31
+
+			cipher += chr(c)
+
+	print 'Pesan terenkripsi: ' + cipher
+
 def main(argv):
 	if (len(sys.argv) != 2):
 		sys.exit('Format: python cipy.py <key>')
